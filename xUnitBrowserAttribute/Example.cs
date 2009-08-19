bb396bc97f243c829e9e8fd139ffe53cac6d1724
unknown <Ben Hall@.(none)>
2009-08-19 03:15:38 +0100

    Had to update the SHDocVw with the signed verison

--- xUnitBrowserAttribute/Example.cs
+++ xUnitBrowserAttribute/Example.cs
@@ -9,13 +9,17 @@ namespace xUnitBrowserAttribute
         [Theory]
         [Browser("IE")]
         [Browser("FireFox")]
-        [Browser("Chrome")]
+        //[Browser("Chrome")]
         public void Should_Click_Search_On_Google_And_Return_Results_For_AspNet(Browser browser)
         {
             browser.GoTo("http://www.google.co.uk");
             browser.TextField(Find.ByName("q")).Value = "asp.net";
             browser.Button(Find.ByName("btnG")).Click();
-            Assert.True(browser.ContainsText("The Official Microsoft ASP.NET Site"));
+
+            string testString = "The Official Microsoft ASP.NET Site";
+            browser.WaitUntilContainsText(testString);
+            Assert.True(browser.ContainsText(testString));
+            browser.Dispose();
         }
     }
 }
